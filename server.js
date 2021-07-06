348621f0e8334b9da2bbbe3004ab7e2e5827f1e7
Dani Botelho <danibl26@gmail.com>
2021-07-06 11:08:04 -0300

    teste rota post Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const routerCuidador = require('./src/routes/cuidador.routes')
 
 const port = process.env.PORT || 8000
 const db = require('./src/data/database')
@@ -13,8 +14,11 @@ app.get('/', (req,res) => {
     res.json({message: 'Deploy no heroku!'})
 })
 
+app.post('/cadastrar' ,routerCuidador.post)
+
 const cuidador = require('./src/routes/cuidador.routes')
 app.use('/cuidador', cuidador)
+
 const tutor = require('./src/routes/tutor.routes')
 app.use('/tutor', tutor)
 
